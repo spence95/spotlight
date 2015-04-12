fdcc13de6630a4e094da559fd3a33bfcf9fdd021
Spencer Sutton <sutton1190@gmail.com>
2015-04-12 11:33:08 -0600

    Version 1.1 done, no bugs found

--- main.js
+++ main.js
@@ -1,26 +1,54 @@
 $(function(){
-    var dogeTop = Math.floor(Math.random()*(screen.height - $("#dogeMeme").height()));
-    var dogeLeft = Math.floor(Math.random()*(screen.width - $("#dogeMeme").width()));
+  //Below function found at
+  //http://stackoverflow.com/questions/18546067/why-is-the-window-width-smaller-than-the-viewport-width-set-in-media-queries
+  function viewport() {
+    var e = window, a = 'inner';
+    if (!('innerWidth' in window )) {
+        a = 'client';
+        e = document.documentElement || document.body;
+    }
+    return { width : e[ a+'Width' ] , height : e[ a+'Height' ] };
+  }
+
+  var vpWidth = viewport().width;
+  var vpHeight = viewport().height;
+
+  var dogeTop = Math.floor(Math.random()*(vpHeight - $("#dogeMeme").height()));
+  var dogeLeft = Math.floor(Math.random()*(vpWidth - $("#dogeMeme").width()));
+
+  $(window).resize(function(){
+    vpWidth = viewport().width;
+    vpHeight = viewport().height;
+    dogeTop = Math.floor(Math.random()*(vpHeight - $("#dogeMeme").height()));
+    dogeLeft = Math.floor(Math.random()*(vpWidth - $("#dogeMeme").width()));
     $("#dogeMeme").css("top", dogeTop);
     $("#dogeMeme").css("left", dogeLeft);
+  });
 
-    $("body").mousemove(function(event){
-      $("#darkness").hide();
+  $("#dogeMeme").css("top", dogeTop);
+  $("#dogeMeme").css("left", dogeLeft);
+
+  $("body").mouseenter(function(event){
+    $("#darkness").hide();
+  });// end mouse enter
+
+  $("body").mousemove(function(event){
       $("#spotlight").show();
       $("#spotlight").css("top", event.pageY);
       $("#spotlight").css("left", event.pageX);
       $("#spotlightCover").css("top", event.pageY - 7);
       $("#spotlightCover").css("left", event.pageX - 7);
-      $("#darknessLeft").css("right",(screen.width -event.pageX));
+      $("#darknessLeft").css("right",(vpWidth -event.pageX));
       $("#darknessLeft").css("width", event.pageX);
-      $("#darknessRight").css("width", (screen.width - event.pageX));
+      $("#darknessRight").css("width", (vpWidth - event.pageX) - $("#spotlight").width());
       $("#darknessRight").css("left", event.pageX + $("#spotlight").width());
       $("#darknessTop").css("height", event.pageY);
       $("#darknessTop").css("left", event.pageX);
-      $("#darknessBottom").css("height", screen.height - event.pageY - 229);
+      $("#darknessBottom").css("height", vpHeight - event.pageY - $("#spotlight").height());
       $("#darknessBottom").css("left", event.pageX);
-      console.log(screen.height);
-      console.log(event.pageY);
-      console.log($("#darknessBottom").css("height"));
-    });//end mouse move
+  });//end mouse move
+
+  $("body").mouseleave(function(event){
+    $("#darkness").show();
+  });
 });//doc ready
