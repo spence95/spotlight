5b3d1a2991501e8940cff8c0cc9819113a293990
Spencer Sutton <sutton1190@gmail.com>
2015-04-11 19:37:18 -0600

    Basic spotlight function

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 $(function(){
-
-});
+    $("#darkness").mousemove(function(event){
+      console.log(event.pageX + " " + event.pageY)
+      $("#spotlight").show();
+      $("#spotlight").css("top", event.pageY);
+      $("#spotlight").css("left", event.pageX);
+    });//end mouse move
+});//doc ready
