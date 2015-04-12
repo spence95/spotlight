1940eaf27299da79dd9f147f535f80a470098538
Spencer Sutton <sutton1190@gmail.com>
2015-04-11 21:46:49 -0600

    Version 1.0 done, still has a few bugs

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 $(function(){
-    $("#darkness").mousemove(function(event){
+    var dogeTop = Math.floor(Math.random()*(screen.height - $("#dogeMeme").height()));
+    var dogeLeft = Math.floor(Math.random()*(screen.width - $("#dogeMeme").width()));
+    $("#dogeMeme").css("top", dogeTop);
+    $("#dogeMeme").css("left", dogeLeft);
+
+    $("body").mousemove(function(event){
+      $("#darkness").hide();
       $("#spotlight").show();
       $("#spotlight").css("top", event.pageY);
       $("#spotlight").css("left", event.pageX);
@@ -11,7 +17,7 @@ $(function(){
       $("#darknessRight").css("left", event.pageX + $("#spotlight").width());
       $("#darknessTop").css("height", event.pageY);
       $("#darknessTop").css("left", event.pageX);
-      $("#darknessBottom").css("height", screen.height - event.pageY - 204);
+      $("#darknessBottom").css("height", screen.height - event.pageY - 229);
       $("#darknessBottom").css("left", event.pageX);
       console.log(screen.height);
       console.log(event.pageY);
